2e1ba9bd0b6923952a9ce30e4e7541747775ede8
Robin Choudhury <xrobinc@gmail.com>
2013-04-04 13:15:33 -0700

    modified:   caesar.c 	modified:   cipher.c

--- cipher.c
+++ cipher.c
@@ -1,8 +1,10 @@
 #include "cipher.h"
 
+//printUsage
+//checkArgs
+
 // encrypt() shifts the characters forward by the amount specified by numShift
-void encrypt(int argc, char *argv[])
-{
+void encrypt(int argc, char *argv[]) {
    int numShift;
    int j;
    char *elements;
@@ -10,39 +12,28 @@ void encrypt(int argc, char *argv[])
    numShift = atoi(argv[2]);
    elements = argv[3];
 
-   for (j = 0; j < strlen(elements); j++)
-   {
+   for (j = 0; j < strlen(elements); j++) {
       // If the character is uppercase and within the first half of the
       // alphabet, you can simply add to it. Subtracting the character value
       // from the last half of the alphabet would result in the same shift.
-      if (elements[j] >= 'A' && elements[j] <= 'M')
-      {
+      if (elements[j] >= 'A' && elements[j] <= 'M') {
          printf("%c", elements[j] + numShift);
-      }
-      else if (elements[j] >= 'N' && elements[j] <= 'Z')
-      {
+      } else if (elements[j] >= 'N' && elements[j] <= 'Z') {
          printf("%c", elements[j] - numShift);
-      }
       // The same method above can be applied in the exact manner for lowercase
       // letters as well.
-      else if (elements[j] >= 'a' && elements[j] <= 'm')
-      {
+      } else if (elements[j] >= 'a' && elements[j] <= 'm') {
          printf("%c", elements[j] + numShift);
-      }
-      else if (elements[j] >= 'n' && elements[j] <= 'z')
-      {
+      } else if (elements[j] >= 'n' && elements[j] <= 'z') {
          printf("%c", elements[j] - numShift);
-      }
-      else
-      {
+      } else {
          printf("%c", elements[j]);
       }
    }
 }
 
 // decrypt() shifts the characters backward by the amount specified by numShift
-void decrypt(int argc, char *argv[])
-{
+void decrypt(int argc, char *argv[]) {
    int numShift;
    int j;
    char *elements;
@@ -50,31 +41,21 @@ void decrypt(int argc, char *argv[])
    numShift = atoi(argv[2]);
    elements = argv[3];
 
-   for (j = 0; j < strlen(elements); j++)
-   {
+   for (j = 0; j < strlen(elements); j++) {
       // If the character is uppercase and within the first half of the
       // alphabet, you can simply add to it. Subtracting the character value
       // from the last half of the alphabet would result in the same shift.
-      if (elements[j] >= 'A' && elements[j] <= 'M')
-      {
+      if (elements[j] >= 'A' && elements[j] <= 'M') {
          printf("%c", elements[j] - numShift);
-      }
-      else if (elements[j] >= 'N' && elements[j] <= 'Z')
-      {
+      } else if (elements[j] >= 'N' && elements[j] <= 'Z') {
          printf("%c", elements[j] + numShift);
-      }
       // The same method above can be applied in the exact manner for lowercase
       // letters as well.
-      else if (elements[j] >= 'a' && elements[j] <= 'm')
-      {
+      } else if (elements[j] >= 'a' && elements[j] <= 'm') {
          printf("%c", elements[j] - numShift);
-      }
-      else if (elements[j] >= 'n' && elements[j] <= 'z')
-      {
+      } else if (elements[j] >= 'n' && elements[j] <= 'z') {
          printf("%c", elements[j] + numShift);
-      }
-      else
-      {
+      } else {
          printf("%c", elements[j]);
       }
    }
